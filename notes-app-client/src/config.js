962e076bb1d2badae1f880f3b133adbe1aaf11e6
Rohan Rao <themathgeek13@gmail.com>
2019-01-03 18:57:15 +0530

    Logout and integration with backend

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,17 @@
+export default {
+	s3: {
+		REGION: "us-east-1",
+		BUCKET: "notes-app-dynamove"
+	},
+	apiGateway: {
+		REGION: "us-east-1",
+		URL: "https://dx3rp7l4uc.execute-api.us-east-1.amazonaws.com/prod"
+	},
+	cognito: {
+		REGION: "us-east-1",
+		USER_POOL_ID: "us-east-1_tha6IVYgw",
+		APP_CLIENT_ID: "30s4b10r28kfllfrhmo7jj5g7p",
+		IDENTITY_POOL_ID: "us-east-1:154c32bc-cc0e-4161-9635-a25dceab7ae4"
+	}
+
+};
